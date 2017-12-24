3da90a0a443049ae04388cbfac919435dd4b2440
dasanjan1296 <dasanjan1296@gmail.com>
2017-12-25 03:46:31 +0530

    Add if.py which consists of all kinds of if statements possible to understand if-else statements.

---
+++ if.py
@@ -0,0 +1,34 @@
+var = 25
+
+if var < 21:
+    print("No Beer Allowed\n")
+
+var = 13
+
+if var < 21:
+    print("No Beer Allowed here\n")
+
+if var is 13:
+    print("You just entered teenage, Come on!\n")
+
+var = "lucy"
+
+if var is "anjan":
+    print("Welcome Admin Sir")
+elif var is "lucy":
+    print("Welcome User Lucy")
+elif var is "roger":
+    print("Welcome User Roger")
+else:
+    print("Please Sign up")
+
+var = "Doug"
+
+if var is "anjan":
+    print("Welcome Admin Sir")
+elif var is "lucy":
+    print("Welcome User Lucy")
+elif var is "roger":
+    print("Welcome User Roger")
+else:
+    print("Please Sign up")
