ce60446b94b4b81efb7b18e147c05866a2c8a402
dasanjan1296 <dasanjan1296@gmail.com>
2017-12-28 02:21:44 +0530

    Add function.py

---
+++ function.py
@@ -0,0 +1,12 @@
+def func1():
+    print("Hello, this is my very first function\n")
+
+def bitcoin_to_inr(btc):
+    amount = btc*927000
+    print(amount)
+
+func1()
+bitcoin_to_inr(13.8)
+bitcoin_to_inr(1.2)
+
+
