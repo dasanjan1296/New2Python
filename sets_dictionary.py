22ea7035213837b06346a500334bce428c8c428b
dasanjan1296 <dasanjan1296@gmail.com>
2017-12-28 02:59:50 +0530

    Add sets and Dictionary examples

---
+++ sets_dictionary.py
@@ -0,0 +1,11 @@
+set1 = {"Anjan","Das","is","cool","is","cool"}
+
+print(set1)
+#The above demonstrates a set in python wherein we cannot have duplicate elements in a set
+
+dict1 = {"Anjan":"My name",'Lucy':'her name','annie':'I dont know'}
+
+for i,j in dict1.items():
+    print(i,j)
+    print("Now observe the difference!")
+    print(i+j)
