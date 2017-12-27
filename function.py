630712c93d9a4987c7cd9d994f5752f499cd875e
dasanjan1296 <dasanjan1296@gmail.com>
2017-12-28 02:47:50 +0530

    Update function.py

--- function.py
+++ function.py
@@ -9,4 +9,45 @@ func1()
 bitcoin_to_inr(13.8)
 bitcoin_to_inr(1.2)
 
+def allowed_dating_age(my_age):
+    girls_age=my_age/2 +7
+    return girls_age
+
+print("You can date girls of age",allowed_dating_age(29),"or elder")
+
+def soc_net(gender="unknown"):
+    if gender is "m":
+        gender="Male"
+    elif gender is "f":
+        gender="Female"
+    print(gender)
+
+soc_net("m")
+soc_net("f")
+soc_net()
+
+def func2(name="Anjan",Occupation="Student",Hobby="Guitar"):
+    print(name,Occupation,Hobby)
+
+func2()
+func2("Amrit","Student","None")
+func2(Hobby="Flute",name="Lenny",Occupation="Student")
+
+def coolfunc(*args):               #Can have any number of arguments
+    total=0
+    for a in args:
+        total += a
+    print(total)
+
+coolfunc(2,3,4,4,5,6)
+coolfunc(2,34,4)
+
+def health(age,apples,ciggy):
+    res = (110-age)+(apples*3.5)-(ciggy*2)
+    print(res)
+
+Anjan_stats = [27,30,0]
+
+health(Anjan_stats[0],Anjan_stats[1],Anjan_stats[2])
+health(*Anjan_stats)
 
